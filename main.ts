6f636ef5a36e82bfea690c164696da94cf99bb59
jbjang-16 <130440085+jbjang-16@users.noreply.github.com>
2023-04-11 16:09:37 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    숫자 += 1
+})
+input.onButtonPressed(Button.B, function () {
+    숫자 += -1
+})
+basic.clearScreen()
+let 숫자 = 0
+basic.forever(function () {
+	
+})
